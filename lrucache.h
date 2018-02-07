7e9d6eff024964791ccbb0b5155b9fd797d51fd0
levizheng(郑纪) <levizheng@tencent.com>
2018-02-07 10:13:28 +0800

    remove debug

--- lrucache.h
+++ lrucache.h
@@ -30,102 +30,102 @@ class LRUCache
 
     public:
 
-    // 需要自动删除cache则设置iCacheLimit为cache总量，否则置为0
-    // 需要设置cache的超时，则设置iTimeLimit为超时时间，按秒计算，否则置为0
-    // 默认不自动清理cache，cache不过期
-    LRUCache(int iCacheLimit = 0, int iTimeLimit = 0)
-        :m_iCacheSize(0),
-        m_iCacheLimit(iCacheLimit),
-        m_iEraseCnt(0),
-        m_iTimeLimit(iTimeLimit)
-    {}
+        // 需要自动删除cache则设置iCacheLimit为cache总量，否则置为0
+        // 需要设置cache的超时，则设置iTimeLimit为超时时间，按秒计算，否则置为0
+        // 默认不自动清理cache，cache不过期
+        LRUCache(int iCacheLimit = 0, int iTimeLimit = 0)
+            :m_iCacheSize(0),
+            m_iCacheLimit(iCacheLimit),
+            m_iEraseCnt(0),
+            m_iTimeLimit(iTimeLimit)
+        {}
 
-    // bUpdate，获取cache时是否更新cache访问时间，默认更新
-    int GetCache(const T & t, Value & value, bool bUpdate = true)
-    {
-        std::lock_guard<std::mutex> lck(mt);
-        HashMapIter mapIter = m_LRUCache.find(t);
-
-        if(mapIter == m_LRUCache.end())
-            return -1;
-
-        mapIter = m_LRUCache.find(t);
-        if(mapIter != m_LRUCache.end())
+        // bUpdate，获取cache时是否更新cache访问时间，默认更新
+        int GetCache(const T & t, Value & value, bool bUpdate = true)
         {
-            if(m_iTimeLimit > 0)
+            std::lock_guard<std::mutex> lck(mt);
+            HashMapIter mapIter = m_LRUCache.find(t);
+
+            if(mapIter == m_LRUCache.end())
+                return -1;
+
+            mapIter = m_LRUCache.find(t);
+            if(mapIter != m_LRUCache.end())
             {
-                time_t tNow = time(NULL);
-                if(tNow - mapIter->second.updatetime > m_iTimeLimit)
+                if(m_iTimeLimit > 0)
                 {
-                    return -2;
+                    time_t tNow = time(NULL);
+                    if(tNow - mapIter->second.updatetime > m_iTimeLimit)
+                    {
+                        return -2;
+                    }
                 }
+                value = mapIter->second.value;
+                if(bUpdate)
+                {
+                    m_List.erase(mapIter->second.listiter);
+                    m_List.push_front(t);
+                    mapIter->second.listiter = m_List.begin();
+                }
+                return m_iCacheSize;
             }
-            value = mapIter->second.value;
-            if(bUpdate)
+            return -3;
+        }
+
+        int UpdateCache(const T & t, const Value & value)
+        {
+            std::lock_guard<std::mutex> lck(mt);
+            HashMapIter mapIter = m_LRUCache.find(t);
+            if(mapIter != m_LRUCache.end())
             {
                 m_List.erase(mapIter->second.listiter);
                 m_List.push_front(t);
                 mapIter->second.listiter = m_List.begin();
+                mapIter->second.updatetime = time(NULL);
+                mapIter->second.value = value;
+                return m_iCacheSize;
             }
-            return m_iCacheSize;
-        }
-        return -3;
-    }
-
-    int UpdateCache(const T & t, const Value & value)
-    {
-        std::lock_guard<std::mutex> lck(mt);
-        HashMapIter mapIter = m_LRUCache.find(t);
-        if(mapIter != m_LRUCache.end())
-        {
-            m_List.erase(mapIter->second.listiter);
-            m_List.push_front(t);
-            mapIter->second.listiter = m_List.begin();
-            mapIter->second.updatetime = time(NULL);
-            mapIter->second.value = value;
-            return m_iCacheSize;
-        }
-        if(m_iCacheLimit > 0)
-        {
-            while(m_iCacheLimit < m_iCacheSize)
+            if(m_iCacheLimit > 0)
             {
-                T t = m_List.back();
-                m_List.pop_back();
-                HashMapIter mapIter = m_LRUCache.find(t);
-                if(mapIter == m_LRUCache.end())
-                    break;
-                m_LRUCache.erase(mapIter);
-                m_iCacheSize--;
-                m_iEraseCnt++;
+                while(m_iCacheLimit < m_iCacheSize)
+                {
+                    T t = m_List.back();
+                    m_List.pop_back();
+                    HashMapIter mapIter = m_LRUCache.find(t);
+                    if(mapIter == m_LRUCache.end())
+                        break;
+                    m_LRUCache.erase(mapIter);
+                    m_iCacheSize--;
+                    m_iEraseCnt++;
+                }
             }
+            m_List.push_front(t);
+            CacheItem mapItem(value, m_List.begin());
+            m_LRUCache.insert(make_pair(t, mapItem));
+            m_iCacheSize++;
+            return m_iCacheSize;
         }
-        m_List.push_front(t);
-        CacheItem mapItem(value, m_List.begin());
-        m_LRUCache.insert(make_pair(t, mapItem));
-        m_iCacheSize++;
-        return m_iCacheSize;
-    }
 
-    int PopCache(T & t, Value & value)
-    {
-        std::lock_guard<std::mutex> lck(mt);
-        if(m_iCacheSize == 0)
-            return -1;
-        t = m_List.back();
-        m_List.pop_back();
-        HashMapIter mapIter = m_LRUCache.find(t);
-        if(mapIter == m_LRUCache.end())
-            return -2;
-        value = mapIter->second.value;
-        m_LRUCache.erase(mapIter);
-        m_iCacheSize--;
-        return 0;
-    }
+        int PopCache(T & t, Value & value)
+        {
+            std::lock_guard<std::mutex> lck(mt);
+            if(m_iCacheSize == 0)
+                return -1;
+            t = m_List.back();
+            m_List.pop_back();
+            HashMapIter mapIter = m_LRUCache.find(t);
+            if(mapIter == m_LRUCache.end())
+                return -2;
+            value = mapIter->second.value;
+            m_LRUCache.erase(mapIter);
+            m_iCacheSize--;
+            return 0;
+        }
 
-    inline int CacheSize()
-    {
-        return m_iCacheSize;
-    }
+        inline int CacheSize()
+        {
+            return m_iCacheSize;
+        }
 
     private:
         List m_List;
